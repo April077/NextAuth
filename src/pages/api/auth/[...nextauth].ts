fb53a3c786da544f40ddd80815f2cbfe6583e9c5
[Tanmay] <tanmaymajee18@gmail.com>
2023-09-02 11:31:57 +0530

    xyz

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,13 +2,11 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
   
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.NEXT_GOOGLE_CLIENT_ID,
       clientSecret: process.env.NEXT_GOOGLE_CLIENT_SECRET,
     }),
-    // ...add more providers here
   ],
   secret: process.env.NEXT_SECRET,
   session: {
@@ -16,7 +14,7 @@ export const authOptions = {
     maxAge: 30 * 24 * 60 * 60,
   },
   jwt: {
-    encryption: true, // Fixed the typo here, changed "encrption" to "encryption"
+    encryption: true,
   },
 };
 
